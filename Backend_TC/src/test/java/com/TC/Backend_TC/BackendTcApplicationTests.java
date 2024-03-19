0913aded245bd1e5bedbd4fd3a403679387709e9
Marcosespa <marcosspa24@gmail.com>
2024-03-19 17:05:28 -0500

    Github implementnacion Spring Booot

---
+++ Backend_TC/src/test/java/com/TC/Backend_TC/BackendTcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TC.Backend_TC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
